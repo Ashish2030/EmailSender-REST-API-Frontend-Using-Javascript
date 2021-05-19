6587aafc3322151a9e3d0179bef0e31b44dda6bf
Ashish2030 <Kumarashish79924@gmail.com>
2021-05-19 08:18:19 +0530

    𝐄𝐦𝐚𝐢𝐥𝐒𝐞𝐧𝐝𝐞𝐫-𝐖𝐞𝐛-𝐀𝐏𝐈-𝐅𝐫𝐨𝐧𝐭𝐞𝐧𝐝-𝐔𝐬𝐢𝐧𝐠-𝐉𝐚𝐯𝐚𝐬𝐜𝐫𝐢𝐩𝐭

---
+++ index.js
@@ -0,0 +1,40 @@
+function send()
+{
+ 
+        var from = document.getElementById("from").value;
+        var password = document.getElementById("password").value;
+        var to = document.getElementById("to").value;
+        var subject = document.getElementById("subject").value;
+        var message = document.getElementById("Message").value;
+         var valid=/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (subject.length===0||message.length===0||!from.match(valid)||!to.match(valid)) 
+        {
+            alert("Invalid Input")
+        }
+        else {
+    
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("POST", "https://email9876.herokuapp.com/data", true);
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+    
+                }
+            };
+    
+    
+            var json = {
+                "from": from,
+                "password": password,
+                "to": to,
+                "subject":subject,
+                "message":message
+            };
+            xhttp.send(JSON.stringify(json));
+
+          
+        }
+    
+        // setTimeout(function () { window.location.reload(); }, 800);
+    
+
+}
